755e33c66eff2d9a020f3f7aa26d57e2a44e1146
Mark <markmarkmagdy1010@gmail.com>
2024-06-29 11:41:51 +0300

    add myApp page in separated folder

---
+++ lib/App/myApp.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+    );
+  }
+}
