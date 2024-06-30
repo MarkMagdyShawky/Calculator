81a79c5eee7f73e688a38cc4c3d0a84c8befb2b8
Mark <markmarkmagdy1010@gmail.com>
2024-06-30 22:27:38 +0300

    build calculator ui

--- lib/Presentation/Home/screens/homePage.dart
+++ lib/Presentation/Home/screens/homePage.dart
@@ -1,6 +1,9 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:calculator/Presentation/Home/widgets/customNumberButton.dart';
+import 'package:calculator/core/resourse/colorManager.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,11 +15,60 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.sizeOf(context).height;
     return Scaffold(
-      backgroundColor: Colors.green,
+      backgroundColor: kFourthColor,
       body: SafeArea(
-        child: Column(),
-      ),
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              CustomNumberButton(number: 'AC', btnColor: kThirdColor),
+              CustomNumberButton(number: 'c', btnColor: kThirdColor),
+              CustomNumberButton(number: '%', btnColor: kThirdColor),
+              CustomNumberButton(number: '/', btnColor: kThirdColor),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              CustomNumberButton(number: '7', btnColor: kPrimaryColor),
+              CustomNumberButton(number: '8', btnColor: kPrimaryColor),
+              CustomNumberButton(number: '9', btnColor: kPrimaryColor),
+              CustomNumberButton(number: 'x', btnColor: kThirdColor),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              CustomNumberButton(number: '4', btnColor: kPrimaryColor),
+              CustomNumberButton(number: '5', btnColor: kPrimaryColor),
+              CustomNumberButton(number: '6', btnColor: kPrimaryColor),
+              CustomNumberButton(number: '-', btnColor: kThirdColor),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              CustomNumberButton(number: '1', btnColor: kPrimaryColor),
+              CustomNumberButton(number: '2', btnColor: kPrimaryColor),
+              CustomNumberButton(number: '3', btnColor: kPrimaryColor),
+              CustomNumberButton(number: '+', btnColor: kThirdColor),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              CustomNumberButton(number: '+/-', btnColor: kThirdColor),
+              CustomNumberButton(number: '0', btnColor: kPrimaryColor),
+              CustomNumberButton(number: '00', btnColor: kPrimaryColor),
+              CustomNumberButton(number: '=', btnColor: kThirdColor),
+            ],
+          ),
+        ],
+      )),
     );
   }
 }
