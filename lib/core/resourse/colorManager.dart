a78dfd8b059035765b17e4329874fdb89e4d0101
Mark <markmarkmagdy1010@gmail.com>
2024-06-30 22:27:15 +0300

    custom color

---
+++ lib/core/resourse/colorManager.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+Color kPrimaryColor = Color(0xffFFF5E0); // background
+Color kSecondaryColor = Color(0xff141E46); // text Color
+Color kThirdColor = Color(0xff8DECB4); //cut btn color
+Color kFourthColor = Color(0xffffffff); //cut btn color
