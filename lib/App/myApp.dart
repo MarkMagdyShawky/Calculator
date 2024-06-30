9045464ff99522d80f2610c0d85fcbace483bc54
Mark <markmarkmagdy1010@gmail.com>
2024-06-30 20:10:29 +0300

    start the app

--- lib/App/myApp.dart
+++ lib/App/myApp.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:calculator/Presentation/Home/screens/homePage.dart';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
@@ -8,7 +9,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
     );
   }
 }
